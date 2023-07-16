7e462a2888b039d4d4d6263faf1a0a82a273d51f
Avanikant <exoduspatronum@gmail.com>
2023-07-16 14:26:31 +0530

    added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,6 +8,7 @@ export default function Main() {
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
+        <li>Can Be easily integrated to github.</li>
       </ul>
     </main>
   );
